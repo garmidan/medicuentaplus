f1fef1d4c81a565d133af773800b5e1afb05d23f
garmidan <garmidan16@gmail.com>
2021-10-11 14:46:24 -0500

    404

--- medicuentasplus/medicuentasplus/settings.py
+++ medicuentasplus/medicuentasplus/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ln8=h4$29%392605ooc^lr0ab6^=bi78dnaczh(e%dlpukaj-@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["medicuentasplus.com"]
 
@@ -50,7 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'medicuentasplus.urls'
 
@@ -133,4 +135,14 @@ FERNET_KEYS = [
     'new key for encrypting',
     'older key for decrypting old data',
 ]
+
+# Secure cookies
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+
+# Set time for expire session
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_COOKIE_AGE = 30
+SESSION_SAVE_EVERY_REQUEST = True
 
--- medicuentasplus/medicuentasplus/wsgi.py
+++ medicuentasplus/medicuentasplus/wsgi.py
@@ -6,11 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
-
+from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medicuentasplus.settings')
-
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(settings.BASE_DIR, 'staticfiles'))
